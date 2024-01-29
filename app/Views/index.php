6b72e412f1760d57fabf6d7f774453f23323652d
Fakkuroqobah <maulanaagung169@gmail.com>
2024-01-29 17:10:10 +0700

    Fix: change asset folder

--- app/Views/index.php
+++ app/Views/index.php
@@ -10,7 +10,7 @@
 
     <title>Ecommerce</title>
 
-    <link href="<?= base_url('vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('asset/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link rel="stylesheet" href="<?= base_url('css/sb-admin-2.min.css') ?>">
@@ -125,9 +125,9 @@
     <i class="fas fa-angle-up"></i>
 </a>
 
-<script src="<?= base_url('vendor/jquery/jquery.min.js') ?>"></script>
-<script src="<?= base_url('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<script src="<?= base_url('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+<script src="<?= base_url('asset/jquery/jquery.min.js') ?>"></script>
+<script src="<?= base_url('asset/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('asset/jquery-easing/jquery.easing.min.js') ?>"></script>
 <script src="<?= base_url('js/sb-admin-2.min.js') ?>"></script>
 <script>
 $(document).ready(function() {
